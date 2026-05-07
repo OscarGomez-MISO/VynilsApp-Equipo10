2de8a1dc1fe22b449862f0bee92cff8ab2c5b024
Oscar Jobany Gomez Ochoa <oj.gomezo1@uniandes.edu.co>
2026-05-06 23:06:00 -0500

    Ajuste ruta archivo coverage reporte sonar - COMPLETADO

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,17 @@ sonar {
         property("sonar.projectKey", System.getenv("SONAR_PROJECT_KEY") ?: "OscarGomez-MISO_VynilsApp-Equipo10")
         property("sonar.organization", System.getenv("SONAR_ORGANIZATION") ?: "oscargomez-miso")
         property("sonar.host.url", "https://sonarcloud.io")
-        property("sonar.coverage.jacoco.xmlReportPaths", "${project.rootDir}/app/build/reports/jacoco/jacocoTestReport/jacocoTestReport.xml")
-        property("sonar.kotlin.binaries", "${project.rootDir}/app/build/tmp/kotlin-classes/debug")
+    }
+}
+
+project(":app") {
+    sonar {
+        properties {
+            property("sonar.sources", "src/main/java")
+            property("sonar.tests", "src/test/java")
+            property("sonar.java.binaries", "build/tmp/kotlin-classes/debug")
+            property("sonar.coverage.jacoco.xmlReportPaths", "build/reports/jacoco/jacocoTestReport/jacocoTestReport.xml")
+            property("sonar.androidLint.reportPaths", "build/reports/lint-results-debug.xml")
+        }
     }
 }
