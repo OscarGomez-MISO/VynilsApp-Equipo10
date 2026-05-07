2eca7b15295f17cc19a5ff0ac75d48283aa4b916
Oscar Jobany Gomez Ochoa <oj.gomezo1@uniandes.edu.co>
2026-05-06 22:38:40 -0500

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,13 +4,3 @@ plugins {
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.sonarqube) apply false
 }
-
-sonar {
-    properties {
-        property("sonar.projectKey", System.getenv("SONAR_PROJECT_KEY") ?: "OscarGomez-MISO_VynilsApp-Equipo10")
-        property("sonar.organization", System.getenv("SONAR_ORGANIZATION") ?: "oscargomez-miso")
-        property("sonar.host.url", "https://sonarcloud.io")
-        property("sonar.coverage.jacoco.xmlReportPaths", "${project.rootDir}/app/build/reports/jacoco/jacocoTestReport/jacocoTestReport.xml")
-        property("sonar.kotlin.binaries", "${project.rootDir}/app/build/tmp/kotlin-classes/debug")
-    }
-}
